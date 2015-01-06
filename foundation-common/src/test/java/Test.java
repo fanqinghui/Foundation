aa654c2199a59024733184d5edd8aa12eed8cee5
fanqinghui <fanqinghui100@126.com>
2015-01-06 19:48:31 +0800

    调整项目的依赖关系与项目结构

---
+++ foundation-common/src/test/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by joey on 15-1-5.
+ */
+public class Test {
+
+    public static void main(String []args){
+        String i=null;
+
+        System.out.println("x".equals(i));
+    }
+}
