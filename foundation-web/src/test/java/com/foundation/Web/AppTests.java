fbae11bbdae81b0c9698692f8f1fda111536fe4f
fanqinghui <fanqinghui100@126.com>
2015-01-06 19:47:57 +0800

    调整项目的依赖关系与项目结构

--- foundation-web/src/test/java/com/foundation/Web/AppTests.java
+++ foundation-web/src/test/java/com/foundation/Web/AppTests.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring-mvc.xml")
 public class AppTests {
     private MockMvc mockMvc;
 
