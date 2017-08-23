c968a46000651022275a9f2d69f97791f6d485d0
FanQinghui <fanqinghui100@126.com>
2017-08-23 15:09:46 +0800

    增加guava Funtures的使用

---
+++ foundation-web/src/test/java/com/foundation/Web/testGuava/GuavaTest.java
@@ -0,0 +1,52 @@
+package com.foundation.Web.testGuava;
+
+import com.google.common.util.concurrent.*;
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+/**
+ * @author fqh
+ * @email fanqinghui100@126.com
+ * @date 2017/8/23 15:00
+ */
+public class GuavaTest {
+
+    @Test
+    public void test(){
+        ListeningExecutorService executorService= MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+
+        ListenableFuture<String> listenableFuture=executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                int i=1;i=i/0;
+                Thread.sleep(1000);
+                return "good luck!!!";
+            }
+        });
+
+
+        Futures.addCallback(listenableFuture, new FutureCallback<String>() {
+
+            @Override
+            public void onSuccess(String s) {
+                System.out.println(s+" onSuccess");
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println(" onFailure:"+throwable.getMessage());
+            }
+        });
+
+
+
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
