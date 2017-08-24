8c8c9a264ee6ba653b9f5187814d2bde00449d1e
FanQinghui <fanqinghui100@126.com>
2017-08-24 14:58:22 +0800

    增加listenableFuture获取方式

--- foundation-web/src/test/java/com/foundation/Web/testGuava/GuavaTest.java
+++ foundation-web/src/test/java/com/foundation/Web/testGuava/GuavaTest.java
@@ -4,6 +4,7 @@ import com.google.common.util.concurrent.*;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 /**
@@ -13,14 +14,15 @@ import java.util.concurrent.Executors;
  */
 public class GuavaTest {
 
-    @Test
-    public void test(){
-        ListeningExecutorService executorService= MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+    ListeningExecutorService executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
 
-        ListenableFuture<String> listenableFuture=executorService.submit(new Callable<String>() {
+    @Test
+    public void test() {
+        ListenableFuture<String> listenableFuture = executorService.submit(new Callable<String>() {
             @Override
             public String call() throws Exception {
-                int i=1;i=i/0;
+                int i = 1;
+                i = i / 0;
                 Thread.sleep(1000);
                 return "good luck!!!";
             }
@@ -31,15 +33,14 @@ public class GuavaTest {
 
             @Override
             public void onSuccess(String s) {
-                System.out.println(s+" onSuccess");
+                System.out.println(s + " onSuccess");
             }
 
             @Override
             public void onFailure(Throwable throwable) {
-                System.out.println(" onFailure:"+throwable.getMessage());
+                System.out.println(" onFailure:" + throwable.getMessage());
             }
         });
-
 
 
         try {
@@ -49,4 +50,27 @@ public class GuavaTest {
         }
 
     }
+
+    @Test
+    public void test2() throws ExecutionException, InterruptedException {
+        ListenableFuture<String> listenableFuture = executorService.submit(new Callable<String>() {
+
+            @Override
+            public String call() throws Exception {
+                return "good luck!!!";
+            }
+        });
+        ListenableFuture<String> listenableFuture2=Futures.transformAsync(listenableFuture, new AsyncFunction<String, String>() {
+            @Override
+            public ListenableFuture<String> apply(String input) throws Exception {
+                System.out.println("input:" + input);
+                return Futures.immediateCheckedFuture("fqh");
+            }
+        });
+        System.out.println(listenableFuture2.get());
+
+    }
+
+    ListeningExecutorService service=MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(3));
+
 }
